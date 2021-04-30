6912fc9934376b40c253880e26a397c86bf5d88d
Erlliam Mejia <tommejas@gmail.com>
2021-04-30 01:52:49 -0400

    Use multiprocessing

--- tools/supa-format.py
+++ tools/supa-format.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import subprocess
+from multiprocessing import Pool
 
 
 def main():
     file_paths = get_file_paths()
+    subprocess_run_args = []
 
     for file_path in file_paths:
         if file_path.startswith("vendor/"):
@@ -13,15 +15,20 @@ def main():
             continue
 
         if file_path.endswith((".c", ".cpp", ".h")):
-            subprocess.run(["clang-format", "-i", file_path])
+            subprocess_run_args.append(["clang-format", "-i", file_path])
         elif file_path.endswith((".html", ".css", ".js")):
-            subprocess.run(["./node_modules/.bin/prettier", "--write", file_path])
+            subprocess_run_args.append(
+                ["./node_modules/.bin/prettier", "--write", file_path]
+            )
         elif file_path.endswith((".py")):
-            subprocess.run(["venv/bin/black", file_path])
+            subprocess_run_args.append(["venv/bin/black", file_path])
         elif file_path.endswith((".go")):
-            subprocess.run(["gofmt", "-w", file_path])
+            subprocess_run_args.append(["gofmt", "-w", file_path])
         elif file_path.endswith((".hs")):
-            subprocess.run(["/home/altair/.local/bin/hindent", file_path])
+            subprocess_run_args.append(["/home/altair/.local/bin/hindent", file_path])
+
+    with Pool(8) as p:
+        p.map(subprocess.run, subprocess_run_args)
 
 
 def get_file_paths():
